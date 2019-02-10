7197a0a02ba16fa84f4567dddddc09dfcc94d665
Philippe Perret <philippe.perret@yahoo.fr>
2019-02-10 10:47:10 +0300

    Correction diverses dans les tests

--- _analyses_/Tests/_tags_.js
+++ _analyses_/Tests/_tags_.js
@@ -1,7 +1,25 @@
 /**
  * Cette analyse est automatiquement lancée au démarrage des tests
  */
-options() ;
+options('code no options') ;
 Tags = `
 acc C_maj x=5 y=5
+`;
+
+
+// Version X.X
+
+Tags = `
+titre Le_Titre_de_l'analyse x=227 y=28
+compositeur Un_anonyme x=492 y=33
+sco sonate_haydn-1.png x=11 y=144 w=17cm
+acc D_Maj x=361 y=109
+har VII*** x=68 y=308
+cadence I type=parfaite x=45 y=274 w=213
+mes 12 x=89 y=130 fs=11px h=11px w=11px
+deg 5# x=605 y=277 c=white bgc=grey
+partie L'Exposition x=169 y=104
+text Un_Texte_Quelconque x=86 y=343 c=#FF0000 bgc=black
+ligne type=U x=71 y=426 w=555
+boite x=493 y=102 c=white bgc=rgba(0,255,0,0.3) h=400
 `;
